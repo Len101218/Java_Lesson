6873cdf6ad2b13bbe1d087fd18a1e4ece466b687
Len101218 <lh.bauer18@gmail.com>
2023-03-15 20:20:30 +0100

    test

---
+++ Tests/Test_task05.java
@@ -0,0 +1,5 @@
+package Tests;
+
+public class Test_task05 {
+    //create a linked List and all its methods in the class Linked_List: use the already implemented skeletton
+}
---
+++ src/Linked_List.java
@@ -0,0 +1,49 @@
+package src;
+
+public class Linked_List {
+    //TODO: change class type of next
+    private insert_something_here next;
+    private int number;
+
+    //TODO: implement Konstruktor
+
+
+
+    public void setNext(){
+        //TODO: return something here
+    }
+    public void setNumber(){
+        //TODO: return something here
+    }
+    public int getNumber(){
+        //TODO: return something here
+    }
+
+    //TODO: change return type here
+    public insert_something_here getNext() {
+        //TODO: return something here
+    }
+
+
+    //TODO: find the 'element' in the linked list 'start'
+    public static void find(Linked_List start, Linked_List element){
+
+    }
+
+    //TODO: remove the 'element' in the linked list 'start'
+    public static void remove(Linked_List start, Linked_List element){
+
+    }
+
+
+    //TODO: return the last element in the linked list 'start'
+    public static insert_something_here getLast(Linked_List start){
+
+    }
+
+
+    //TODO: remove all after 'element' in the linked list 'start' ('element' will be the new last element)
+    public static insert_something_here removeAfter(Linked_List start, Linked_List element){
+
+    }
+}
